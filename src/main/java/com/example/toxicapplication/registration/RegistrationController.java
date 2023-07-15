2862126c0aa6160e7e24093ea14ee22a4d252432
tyz01 <jenaynogach1@mail.ru>
2023-07-16 00:02:51 +0300

    change request on server (fix status 405) 2

--- src/main/java/com/example/toxicapplication/registration/RegistrationController.java
+++ src/main/java/com/example/toxicapplication/registration/RegistrationController.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping(value = "/registration", method = RequestMethod.POST)
-@RequestMapping(path = "api/v1/registration", method = RequestMethod.POST)
+@RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
@@ -13,10 +12,6 @@ public class RegistrationController {
     @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
-    }
-    @GetMapping(path = "confirm")
-    public String confirm(@RequestParam("token") String token) {
-        return registrationService.confirmToken(token);
     }
 
 }
--- src/main/java/com/example/toxicapplication/registration/RegistrationService.java
+++ src/main/java/com/example/toxicapplication/registration/RegistrationService.java
@@ -48,7 +48,7 @@ public class RegistrationService {
 //        // Update the user's account with the new access token
 //        appUserService.updateAccessToken(request.getUserName(), newAccessToken);
 
-        String link = "https://bb1a-37-214-30-21.ngrok.io/api/v1/registration/confirm?token=" + token;
+        String link = "http://localhost:8080/registration/confirm?token=" + token;
         emailSender.send(
                 request.getEmail(),
                 buildEmail(request.getUserName(), link));
