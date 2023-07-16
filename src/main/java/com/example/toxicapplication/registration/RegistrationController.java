32b811fa844b4ce9fec8f7762e50e3d80d05b502
tyz01 <jenaynogach1@mail.ru>
2023-07-16 10:17:53 +0300

    change request on server (fix status 405) 10

--- src/main/java/com/example/toxicapplication/registration/RegistrationController.java
+++ src/main/java/com/example/toxicapplication/registration/RegistrationController.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "api/v1/registration")
+@RequestMapping(path = "api/v1/registration", produces = "application/json")
 @AllArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
 
-    @PostMapping(produces = "application/json")
+    @PostMapping()
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
