2be4d28130af96d39c8800212f478412d603195b
tyz01 <jenaynogach1@mail.ru>
2023-12-29 22:16:07 +0300

    add configuration v11

--- src/main/java/com/example/googleMapApplicationTracker/login/LoginController.java
+++ src/main/java/com/example/googleMapApplicationTracker/login/LoginController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/login")
+@CrossOrigin(origins = "https://gamefool.gamefi.codes")
 @AllArgsConstructor
 public class LoginController {
     private AuthenticationManager authenticationManager;
