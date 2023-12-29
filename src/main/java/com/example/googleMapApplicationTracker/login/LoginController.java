6b1267b5d5f74d7710d187f4d2f373af672bb4bb
tyz01 <jenaynogach1@mail.ru>
2023-12-29 15:53:52 +0300

    change login

--- src/main/java/com/example/googleMapApplicationTracker/login/LoginController.java
+++ src/main/java/com/example/googleMapApplicationTracker/login/LoginController.java
@@ -1,19 +1,23 @@
 package com.example.googleMapApplicationTracker.login;
 
+import com.example.googleMapApplicationTracker.appUser.repository.AppUserRepository;
 import com.example.googleMapApplicationTracker.appUser.utility.ApiResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/login")
 @AllArgsConstructor
 public class LoginController {
     private AuthenticationManager authenticationManager;
+    private AppUserRepository appUserRepository;
 
     @CrossOrigin
     @GetMapping()
@@ -26,17 +30,19 @@ public class LoginController {
                     userName,
                     password
             );
-
+            var existsUser = appUserRepository.findByUserName(userName).orElseThrow(
+                    () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+            );
             Authentication authenticated = authenticationManager.authenticate(authentication);
             if (!authenticated.isAuthenticated()) {
                 return new ApiResponse<>("user not authenticated", true);
             }
 
             SecurityContextHolder.getContext().setAuthentication(authenticated);
+            return new ApiResponse<>("Success!", false, existsUser.getId(), existsUser.getUsername(), null);
 
         } catch (AuthenticationException e) {
             return new ApiResponse<>("check username or password", true);
         }
-        return null;
     }
 }
--- src/main/java/com/example/googleMapApplicationTracker/registration/RegistrationService.java
+++ src/main/java/com/example/googleMapApplicationTracker/registration/RegistrationService.java
@@ -13,15 +13,16 @@ import org.springframework.stereotype.Service;
 public class RegistrationService {
     private final AppUserService appUserService;
     private final AppUserRepository appUserRepository;
+
     public ApiResponse<String> register(RegistrationRequest request) {
         if (appUserRepository.existsByUserName(request.getUserName())) {
             return new ApiResponse<>("Username already exists", true);
         }
-var newUser = new AppUser(
-        request.getUserName(),
-        request.getPassword(),
-        AppUserRole.USER
-);
+        var newUser = new AppUser(
+                request.getUserName(),
+                request.getPassword(),
+                AppUserRole.USER
+        );
         appUserService.signUpUser(newUser);
 
         return new ApiResponse<>("Success!", false, newUser.getId(), request.getUserName(), null);
